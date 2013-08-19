79ca1ba4330819441a1626fda320ab9ffd587a29
Jared Phillips <jaredtphillips@me.com>
2013-08-19 01:16:39 -0400

    adding a contact

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,12 +7,23 @@ class ContactsController < ApplicationController
   end
 
   def create
+  	@contact = Contact.new(contacts_params)
+  	if @contact.save
+  		redirect_to user_contacts_path(@user)
+  	else
+  		render :new
+  	end	
   end
 
   def index
+  	@contacts = Contact.find_by(params[:user_id])
   end
 
 private
+
+  def contacts_params
+  	params.require(:contact).permit(:firstname, :lastname, :email, :phone, :notes)
+	end
 
   def load_user
 		@user = User.find(params[:user_id])
